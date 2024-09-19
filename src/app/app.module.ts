75db7fae72e31cd59093381b8f0392dc9f6b7764
Daniel Teran <daniel_teran@javeriana.edu.co>
2024-09-18 20:25:30 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './landing/header/header.component';
+import { FooterComponent } from './landing/footer/footer.component';
+import { HomeComponent } from './landing/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
